288a1834cb480ae2f925de9889acccdd8045e25c
Aniket Kumbhar <akumbhar@ch.iitr.ac.in>
2020-06-10 00:58:11 +0530

    no static files

--- safety/settings.py
+++ safety/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'tkf26t9(*1iqg6tkmdx9#o-*v4im3(bg0jj@t&hy!oig2&ya-p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '127.0.0.1','abkumbhar.pythonanywhere.com']
+ALLOWED_HOSTS = [ '127.0.0.1','abkumbhar.pythonanywhere.com','suraksha-udyog.herokuapp.com']
 
 
 # Application definition
@@ -140,14 +140,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ORIGIN_ALLOW_ALL = True
 django_heroku.settings(locals())
